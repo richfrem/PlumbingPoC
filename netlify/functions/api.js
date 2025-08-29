7b8aa099abf8a544d213fb237f55a94310045732
richfrem <richard.fremmerlid@gov.bc.ca>
2025-08-29 09:56:25 -0700

    refactor: Consolidate package.json to root for Netlify functions

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,7 +2,7 @@
 
 const serverless = require('serverless-http');
 // This line imports the Express app you just refactored.
-const app = require('../../api/server');
+const app = require('../vite-app/api/server');
 
 // This wraps your Express app for Netlify and exports it as a handler.
 module.exports.handler = serverless(app);
